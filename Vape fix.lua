98f0fe41547e90bf488db16c6d15ad648ac0b725
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2022-11-12 19:51:42 -0500

    Create Vape fix.lua

---
+++ Vape fix.lua
@@ -0,0 +1,3 @@
+queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+queueteleport('loadstring(game:HttpGet("https://", true))()')
