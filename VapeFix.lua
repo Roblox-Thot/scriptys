bb262ebf1f3ff3a892f6bece25f8fb303f8357d0
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2022-11-12 19:54:59 -0500

    Update VapeFix.lua

--- VapeFix.lua
+++ VapeFix.lua
@@ -1,3 +1,3 @@
 queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
-queueteleport('loadstring(game:HttpGet("https://https://github.com/Roblox-Thot/scriptys/raw/main/VapeFix.lua", true))()')
+queueteleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/Roblox-Thot/scriptys/main/VapeFix.lua", true))()')
